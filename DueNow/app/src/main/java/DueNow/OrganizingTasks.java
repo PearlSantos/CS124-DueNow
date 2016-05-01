42a5d520599c9d46f1e7d7aad5d4de264cffb346
ejcris <elysiajelenavilladarez@yahoo.com>
2016-05-01 12:06:08 +0800

    Adjustments to the algorithm

--- DueNow/app/src/main/java/DueNow/OrganizingTasks.java
+++ DueNow/app/src/main/java/DueNow/OrganizingTasks.java
@@ -34,8 +34,6 @@ public class OrganizingTasks {
             newTask.setRecommendedStartTime(recStartTime);
             newTask.setRecommendedTimeFinish();
             newList.add(newTask);
-            System.out.println("REC START TIME: " + dateFormat.format(newTask.getRecommendedStartTime()));
-            System.out.println("REC FINISH TIME: " + dateFormat.format(newTask.getRecommendedTimeFinish()));
         } else {
             //  Calendar setRecStartTime = (Calendar) currTime.clone();
             //  setRecStartTime.add(Calendar.HOUR, intervalInHour / 2);
@@ -43,27 +41,14 @@ public class OrganizingTasks {
             while (indexOfList < list.size()) {
                 Task analyzeNow = list.get(indexOfList);
                 if (getIntervalToDeadline(analyzeNow, currTime) > getIntervalToDeadline(newTask, currTime)) {
-                    recStartTime = (Calendar) analyzeNow.getRecommendedTimeFinish().clone();
-                    recStartTime.add(Calendar.MINUTE, REST_TIME);
-                    newTask.setRecommendedStartTime(recStartTime);
-                    newTask.setRecommendedTimeFinish();
-                    newList.add(newTask);
+                    newList.add(setRecStartTime(analyzeNow, newTask));
                     adjustRecTime(newList, indexOfList);
                 } else if (getIntervalToDeadline(analyzeNow, currTime) == getIntervalToDeadline(newTask, currTime)) {
                     if (analyzeNow.getPriority() > newTask.getPriority()) {
-                        recStartTime = (Calendar) analyzeNow.getRecommendedTimeFinish().clone();
-                        recStartTime.add(Calendar.MINUTE, REST_TIME);
-                        newTask.setRecommendedStartTime(recStartTime);
-                        newTask.setRecommendedTimeFinish();
-                        newList.add(newTask);
+                        newList.add(setRecStartTime(analyzeNow, newTask));
                         adjustRecTime(newList, indexOfList);
                     } else {
-                        newList.add(analyzeNow);
-                        recStartTime = (Calendar) analyzeNow.getRecommendedTimeFinish().clone();
-                        recStartTime.add(Calendar.MINUTE, REST_TIME);
-                        newTask.setRecommendedStartTime(recStartTime);
-                        newTask.setRecommendedTimeFinish();
-                        newList.add(newTask);
+                        newList.add(setRecStartTime(analyzeNow, newTask));
                         indexOfList++;
                         adjustRecTime(newList, indexOfList);
                     }
@@ -73,12 +58,9 @@ public class OrganizingTasks {
                     indexOfList++;
                 }
             }
+            //if add to last index
             if(listSize == newList.size()){
-                recStartTime = (Calendar) newList.get(newList.size()-1).getRecommendedTimeFinish().clone();
-                recStartTime.add(Calendar.MINUTE, REST_TIME);
-                newTask.setRecommendedStartTime(recStartTime);
-                newTask.setRecommendedTimeFinish();
-                newList.add(newTask);
+                newList.add(setRecStartTime(newList.get(newList.size()-1), newTask));
             }
         }
 
@@ -88,19 +70,32 @@ public class OrganizingTasks {
 
     public void adjustRecTime(ArrayList<Task> newList, int indexStart) {
         while (indexStart < list.size()) {
-            Task prevTask = list.get(indexStart-1);
-            Task nowTask = list.get(indexStart);
-            Calendar recStartTime = (Calendar) prevTask.getRecommendedTimeFinish().clone();
-            recStartTime.add(Calendar.MINUTE, REST_TIME);
-            nowTask.setRecommendedStartTime(recStartTime);
-            nowTask.setRecommendedTimeFinish();
-            newList.add(nowTask);
+            Task prevTask;
+            Task nowTask;
+            if(indexStart > 0) {
+                prevTask = list.get(indexStart - 1);
+                nowTask = list.get(indexStart);
+            } else{
+                prevTask = newList.get(0);
+                nowTask = list.get(0);
+            }
+            newList.add(setRecStartTime(prevTask, nowTask));
             indexStart++;
         }
     }
 
+    public Task setRecStartTime(Task analyzeNow, Task nowTask){
+        Calendar recStartTime = (Calendar) analyzeNow.getRecommendedTimeFinish().clone();
+        recStartTime.add(Calendar.MINUTE, REST_TIME);
+        nowTask.setRecommendedStartTime(recStartTime);
+        nowTask.setRecommendedTimeFinish();
+        return nowTask;
+    }
+
     public int getIntervalToDeadline(Task t, Calendar currTime) {
+        //System.out.println("Deadline: " + dateFormat.format(t.deadline.getTime()));
         long milli = t.getDeadline().getTime().getTime() - currTime.getTime().getTime();
+      //  long milli = currTime.getTime().getTime() - currTime.getTime().getTime();
         int hour = (int) milli / (60 * 60 * 1000) % 24;
         return hour;
     }
--- DueNow/app/src/main/java/DueNow/TestMain.java
+++ DueNow/app/src/main/java/DueNow/TestMain.java
@@ -1,5 +1,10 @@
 package DueNow;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import DueNow.decoratorfactory.TaskSimpleFactory;
 import DueNow.decoratorfactory.difficulty.EasyDifficulty;
 import DueNow.decoratorfactory.difficulty.HardDifficulty;
@@ -10,14 +15,51 @@ import DueNow.decoratorfactory.difficulty.MedDifficulty;
  */
 public class TestMain {
     public static void main(String args[]){
+
+        final SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, EEE, hh:mm a");
         TaskSimpleFactory f = new TaskSimpleFactory();
         Task t = f.createTask("Homework", 10);
-        System.out.println(t.getTimeNeeded());
-        t = new EasyDifficulty(t);
-        System.out.println(t.getTimeNeeded());
-        t = new MedDifficulty(t);
-        System.out.println(t.getTimeNeeded());
-        t = new HardDifficulty(t);
-        System.out.println(t.getTimeNeeded());
+        Calendar deadline = new GregorianCalendar();
+        deadline.set(2016, 4, 1, 13, 30);
+        t.setName("Homework in Th131");
+        t.setDescription("MUST do to get an A+++");
+        t.setDeadline(deadline);
+        t.setPriority(3);
+        OrganizingTasks o = new OrganizingTasks();
+        o.addTask(t);
+
+        Task t2 = f.createTask("Quiz", 10);
+        Calendar deadline2 = new GregorianCalendar();
+        deadline2.set(2016, 4, 2, 13, 30);
+        t2.setName("Quiz in CS124");
+        t2.setDescription("MUST do to get an A+++");
+        t2.setDeadline(deadline2);
+        t2.setPriority(3);
+        o.addTask(t2);
+
+        Task t3 = f.createTask("Orals", 10);
+        Calendar deadline3 = new GregorianCalendar();
+        deadline3.set(2016, 4, 1, 13, 30);
+        t3.setName("Philo orals");
+        t3.setDescription("MUST do to get an A+++");
+        t3.setDeadline(deadline3);
+        t3.setPriority(2);
+        o.addTask(t3);
+
+        ArrayList<Task> list = o.getList();
+        for(Task h: list){
+            System.out.println("Name: " + h.getName());
+            System.out.println("Deadline: " + sdf.format(h.getDeadline().getTime()));
+            System.out.println("Recommended Start Time " + sdf.format(h.getRecommendedStartTime().getTime()));
+            System.out.println("Recommended Finish Time " + sdf.format(h.getRecommendedTimeFinish().getTime()));
+        }
+//
+//        t = new EasyDifficulty(t);
+//        System.out.println(t.getTimeNeeded());
+//        t = new MedDifficulty(t);
+//        System.out.println(t.getTimeNeeded());
+//        t = new HardDifficulty(t);
+//        System.out.println(t.getTimeNeeded());
+
     }
 }
